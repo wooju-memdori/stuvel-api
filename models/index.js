05f5a0d93dff3c9ccce1302fe6e24b1534217186
Hee-jin506 <gmlwls35220@gmail.com>
2021-07-25 21:13:15 +0900

    basic setting

---
+++ models/index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require("path");
+
+dotenv.config({
+    path: path.resolve(
+      process.cwd(),
+      process.env.NODE_ENV == "production" ? ".env" : ".env.dev"
+    ),
+  });
+
+const connect = () => {
+    if (process.env.NODE_ENV !== 'production') {
+        mongoose.set('debug', true);
+    }
+
+    mongoose.connect(process.env.mongoURI, {
+        dbName: 'nodejs',
+        uesNewUrlParser: true,
+        useCreateIndex: true,
+    }, (error) => {
+        if (error) {
+            console.log('몽고db 연결 에러', error);
+        } else {
+            console.log('몽고db 연결 성공', error);
+        }
+    });
+};
+
+mongoose.connection.on('error', (error) => {
+    console.log('몽고db 연결 에러', error);
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('몽고db 연결이 끊어졌습니다. 연결을 재시도합니다.');
+    connect();
+})
+
+module.exports = connect;
