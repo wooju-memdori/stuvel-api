0a25adf7b6ea9e846e477a470096677e38c50c71
hayeon17kim <hayeon17kim@gmail.com>
2021-07-29 16:14:09 +0900

    add : server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  // 홈페이지로 갈 때마다 랜덤한 uuid(roomId) 생성
+  res.redirect(`${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId); // send a message to the all in the room except me
+  });
+});
+
+server.listen(3000);
