e4ff211dd17db6a98c77915e72bd0aa14d5aaf49
hayeon17kim <hayeon17kim@gmail.com>
2021-08-03 21:22:45 +0900

    delete : unnecessary codes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const { v4: uuidV4 } = require("uuid");
-const rooms = [];
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
