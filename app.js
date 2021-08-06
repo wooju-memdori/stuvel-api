e690a8fd7dfd16dcfe4d980c1276562a8d25b72d
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2021-08-07 02:44:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.set('view engine', 'ejs');
+
 // 라우터
 app.use('/', indexRouter);
 app.use('/room', roomRouter);
