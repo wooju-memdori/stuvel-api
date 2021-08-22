aae03da0e96fa8d72675daccca6749b017af56c4
Hayeon Kim <hayeon17kim@gmail.com>
2021-08-23 01:33:07 +0900

    add : GET following, follower router

--- routes/follow.js
+++ routes/follow.js
@@ -1,13 +1,73 @@
 const express = require('express');
-const { v4: uuidV4 } = require('uuid');
-const { Op } = require('sequelize');
 const Follow = require('../models/Follow');
+const User = require('../models/User');
 
 const router = express.Router();
 
-// 매칭(기존 방에서 자리를 찾고 없으면 방 새로 생성)
-router.get('/followee', async (req, res) => {
-  console.log(req.user);
+router.get('/followers', async (req, res) => {
+  Follow.findAll({
+    include: [
+      {
+        model: User,
+        as: 'followers',
+        attributes: ['id', 'email'],
+      },
+    ],
+    where: { targetId: req.user.id },
+  })
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(err);
+    });
+});
+
+router.get('/followings', async (req, res) => {
+  Follow.findAll({
+    include: [
+      {
+        model: User,
+        as: 'following',
+        attributes: ['id', 'email'],
+      },
+    ],
+    where: { targetId: req.user.id },
+  })
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(err);
+    });
+});
+
+router.post('/follow/:id', async (req, res) => {
+  Follow.create({
+    subjectId: req.user.dataValues.id,
+    targetId: req.params.id,
+  })
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(err);
+    });
+});
+
+router.delete('/follow/:id', async (req, res) => {
+  Follow.delete({
+    where: {
+      subjectId: req.user.dataValues.id,
+      targetId: req.params.id,
+    },
+  }).catch(err => {
+    console.log(err);
+    res.send(err);
+  });
 });
 
 module.exports = router;
