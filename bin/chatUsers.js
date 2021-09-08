e987cfe2bcb5716f75b45c881520173e0fe6a01e
Hayeon Kim <hayeon17kim@gmail.com>
2021-09-09 00:47:49 +0900

    add : chat users remove users

--- bin/chatUsers.js
+++ bin/chatUsers.js
@@ -15,8 +15,17 @@ const addUser = ({ id, name, room }) => {
   return { user };
 };
 
+const removeUser = id => {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+  return 0;
+};
+
 const getUser = id => users.find(user => user.id === id);
 
 const getUsersInRoom = room => users.filter(user => user.room === room);
 
-module.exports = { addUser, getUser, getUsersInRoom };
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
